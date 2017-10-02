170c9a9c4275de6e71ac1eae7216dd6b0353dea6
Justine Meyer <dangermonkey@users.noreply.github.com>
2017-10-02 15:39:49 -0400

    added urls to randomizer

--- js/randomizer.js
+++ js/randomizer.js
@@ -1,6 +1,7 @@
 function getRandomVideo() {
 
-var randomVideo = ['b.html', 'c.html', 'e.html']
+var randomVideo = ['b.html', 'c.html', 'e.html', 'h.html', 'i.html', 'j.html', 'k.html', 'r.html', 's.html', 'u.html', _
+'v.html', 'w.html', 'y.html']
 
 var rand = randomVideo[Math.floor(Math.random() * randomVideo.length)];
 
