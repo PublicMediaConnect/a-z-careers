cb47a9d1e9cdf5d0d70e36e8bd9c79ee77e0fad9
Justine Meyer <dangermonkey@users.noreply.github.com>
2017-10-02 15:55:32 -0400

    fixed string issue

--- js/randomizer.js
+++ js/randomizer.js
@@ -1,7 +1,6 @@
 function getRandomVideo() {
 
-var randomVideo = ['b.html', 'c.html', 'e.html', 'h.html', 'i.html', 'j.html', 'k.html', 'r.html', 's.html', 'u.html', _
-'v.html', 'w.html', 'y.html']
+var randomVideo = ['b.html', 'c.html', 'e.html', 'h.html', 'i.html', 'j.html', 'k.html', 'r.html', 's.html', 'u.html', 'v.html', 'w.html', 'y.html']
 
 var rand = randomVideo[Math.floor(Math.random() * randomVideo.length)];
 
